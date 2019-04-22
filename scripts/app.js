ed4c64cb1e73f705b03ae4403a8782827c610c94
Ruheni Alex <aleruheni@outlook.com>
2019-04-22 21:01:13 +0300

    created functions for creating container, upload, delete and list blob
    created function to create element

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,104 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 
+const account = {
+    name: 'quinn',
+    sas: 'se=2019-06-01&sp=rwdlac&sv=2018-03-28&ss=b&srt=sco&sig=TzbhTXyTEQRmkcDN6zoHWgoVYRBDqdEaX2rmV0iTINA%3D',
+};
+
+const blobUri = `https://${account.name}.blob.core.windows.net`;
+const blobService = AzureStorage.Blob.createBlobServiceWithSas(blobUri, account.sas);
+
+// Create a container on azure
+const createContainer = () => {
+    document.getElementById('create-button').addEventListener('click', () => {
+        blobService.createContainerIfNotExists('container', (err, container) => {
+            if (err) {
+                console.log('The following error occured when creating container: ', err);
+            } else {
+                console.log(container.name);
+                alert(`${container.name} exists already.`);
+            }
+        });
+    });
+};
+
+// Upload a blob file
+const uploadBlob = () => {
+    document.getElementById('upload-button').addEventListener('click', () => {
+        const file = document.getElementById('fileinput').files[0];
+
+        blobService.createBlockBlobFromBrowserFile('container', file.name, file, (err, result) => {
+            if (err) {
+                console.log('The following error occured while uploading blob: ', err);
+            } else {
+                console.log('Upload is successful');
+                alert('Upload successful.');
+            }
+        });
+    });
+};
+
+// List Blobs
+const listBlob = () => {
+    document.getElementById('list-button').addEventListener('click', () => {
+        // const blobs = document.getElementById('blobs');
+        blobService.listBlobsSegmented('container', null, (err, results) => {
+            if (err) {
+                console.log('The following error occured while fetching blob: ', err);
+            } else {
+                results.entries.forEach(blob => {
+                    console.log(blob.name);
+                });
+            }
+        });
+    });
+};
+
+// Delebe Blobs
+const deleteBlob = () => {
+    document.getElementById('delete-button').addEventListener('click', () => {
+        let blobName;
+        blobService.deleteBlobIfExists('container', blobName, (err, result) => {
+            if (err) {
+                console.log('The following error occured while deleting blob: ', err);
+            } else {
+                console.log('Blob deleted successfully');
+            }
+        });
+    });
+};
+
 const record = document.querySelector('.record');
 const stop = document.querySelector('.stop');
 const soundClips = document.querySelector('.sound-clips');
+
+const createEl = () => {
+    const clipName = prompt('Enter name for your audio clip');
+
+    const clipContainer = document.createElement('article');
+    const clipLabel = document.createElement('p');
+    const audio = document.createElement('audio');
+    const deleteButton = document.createElement('button');
+    const uploadButton = document.createElement('button');
+
+    clipContainer.classList.add('clip');
+    audio.setAttribute('controls', '');
+    uploadButton.textContent = 'Upload';
+    deleteButton.innerHTML = 'Delete';
+    clipLabel.innerHTML = clipName;
+
+    // clipContainer.setAttribute('class', 'row');
+    uploadButton.setAttribute('id', 'upload-button');
+    uploadButton.setAttribute('class', 'btn blue darken-1 right-align button');
+    deleteButton.setAttribute('class', 'btn blue darken-1 right-align button');
+
+    clipContainer.appendChild(audio);
+    clipContainer.appendChild(clipLabel);
+    clipContainer.appendChild(uploadButton);
+    clipContainer.appendChild(deleteButton);
+    soundClips.appendChild(clipContainer);
+};
 
 /**
  * Basic getUserMedia setup
@@ -46,30 +141,7 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
                 mediaRecorder.onstop = e => {
                     console.log('recorder stopped');
 
-                    const clipName = prompt('Enter name for your audio clip');
-
-                    const clipContainer = document.createElement('article');
-                    const clipLabel = document.createElement('p');
-                    const audio = document.createElement('audio');
-                    const deleteButton = document.createElement('button');
-                    const uploadButton = document.createElement('button');
-
-                    clipContainer.classList.add('clip');
-                    audio.setAttribute('controls', '');
-                    uploadButton.textContent = 'Upload';
-                    deleteButton.innerHTML = 'Delete';
-                    clipLabel.innerHTML = clipName;
-
-                    // clipContainer.setAttribute('class', 'row');
-                    uploadButton.setAttribute('class', 'btn blue darken-1 right-align button');
-                    deleteButton.setAttribute('class', 'btn blue darken-1 right-align button');
-
-                    clipContainer.appendChild(audio);
-                    clipContainer.appendChild(clipLabel);
-                    clipContainer.appendChild(uploadButton);
-                    clipContainer.appendChild(deleteButton);
-                    soundClips.appendChild(clipContainer);
-
+                    createEl();
                     const blob = new Blob(chunks, {
                         type: 'audio/ogg; codecs=opus',
                     });
@@ -84,7 +156,9 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
                     };
 
                     // listen for file selection
-                    uploadButton.onclick = () => {};
+                    uploadButton.onclick = () => {
+                        uploadBlob();
+                    };
                 };
             },
             // Error callback
@@ -95,64 +169,3 @@ if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     console.log('Sorry, getUserMedia not supported on your browser!');
     alert('Sorry, getUserMedia not supported on your browser!');
 }
-
-const account = {
-    name: 'quinn',
-    sas: 'se=2019-06-01&sp=rwdlac&sv=2018-03-28&ss=b&srt=sco&sig=TzbhTXyTEQRmkcDN6zoHWgoVYRBDqdEaX2rmV0iTINA%3D',
-};
-
-const blobUri = `https://${account.name}.blob.core.windows.net`;
-const blobService = AzureStorage.Blob.createBlobServiceWithSas(blobUri, account.sas);
-
-// Create a container on azure
-document.getElementById('create-button').addEventListener('click', () => {
-    blobService.createContainerIfNotExists('container', (err, container) => {
-        if (err) {
-            console.log('The following error occured when creating container: ', err);
-        } else {
-            console.log(container.name);
-        }
-    });
-});
-
-// Upload a blob file
-document.getElementById('upload-button').addEventListener('click', () => {
-    const file = document.getElementById('fileinput').files[0];
-
-    blobService.createBlockBlobFromBrowserFile('container', file.name, file, (err, result) => {
-        if (err) {
-            console.log('The following error occured while uploading blob: ', err);
-        } else {
-            console.log('Upload is successful');
-        }
-    });
-});
-
-// List Blobs
-document.getElementById('list-button').addEventListener('click', () => {
-    // const blobs = document.getElementById('blobs');
-    blobService.listBlobsSegmented('container', null, (err, results) => {
-        if (err) {
-            console.log('The following error occured while fetching blob: ', err);
-        } else {
-            results.entries.forEach(blob => {
-                console.log(blob.name);
-                // let blobEl = document.createElement('li');
-                // blobEl = blob.name;
-                // blobs.append(blobEl);
-            });
-        }
-    });
-});
-
-// Delebe Blobs
-document.getElementById('delete-button').addEventListener('click', () => {
-    let blobName;
-    blobService.deleteBlobIfExists('container', blobName, (err, result) => {
-        if (err) {
-            console.log('The following error occured while deleting blob: ', err);
-        } else {
-            console.log('Blob deleted successfully');
-        }
-    });
-});
