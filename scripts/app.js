b3e880a1e579b2c897b076464e2d4ef572b4c239
Ruheni Alex <aleruheni@outlook.com>
2019-04-22 11:47:04 +0300

    added media recording to app.js ,.
    yet to merge Azure storage account functionality with media recorder

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,101 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
+
+const record = document.querySelector('.record');
+const stop = document.querySelector('.stop');
+const soundClips = document.querySelector('.sound-clips');
+
+/**
+ * Basic getUserMedia setup
+ */
+if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
+    console.log('getUserMedia supported');
+    navigator.mediaDevices
+        .getUserMedia({ audio: true })
+        .then(
+            stream => {
+                // create new instance of MediaRecorder() and pass stream to be captured
+                const mediaRecorder = new MediaRecorder(stream);
+
+                // start recording the stream once record button is pressed
+                record.onclick = () => {
+                    mediaRecorder.start();
+                    console.log(mediaRecorder.state);
+                    console.log('recorder started');
+                    // record.style.background = 'red';
+                    record.style.color = 'red';
+                };
+
+                /**
+                 * collect audio data as recording progresses with
+                 * on mediaRecorder.ondataavailable event
+                 */
+                let chunks = [];
+                mediaRecorder.ondataavailable = e => chunks.push(e.data);
+
+                // finalize Blob ready for use in app
+                stop.onclick = () => {
+                    mediaRecorder.stop();
+                    console.log(mediaRecorder.state);
+                    console.log('recorder stopped');
+                    record.style.background = '';
+                    record.style.color = '';
+                };
+
+                // grabbing and using the blob
+                mediaRecorder.onstop = e => {
+                    console.log('recorder stopped');
+
+                    const clipName = prompt('Enter name for your audio clip');
+
+                    const clipContainer = document.createElement('article');
+                    const clipLabel = document.createElement('p');
+                    const audio = document.createElement('audio');
+                    const deleteButton = document.createElement('button');
+                    const uploadButton = document.createElement('button');
+
+                    clipContainer.classList.add('clip');
+                    audio.setAttribute('controls', '');
+                    uploadButton.textContent = 'Upload';
+                    deleteButton.innerHTML = 'Delete';
+                    clipLabel.innerHTML = clipName;
+
+                    // clipContainer.setAttribute('class', 'row');
+                    uploadButton.setAttribute('class', 'btn blue darken-1 right-align button');
+                    deleteButton.setAttribute('class', 'btn blue darken-1 right-align button');
+
+                    clipContainer.appendChild(audio);
+                    clipContainer.appendChild(clipLabel);
+                    clipContainer.appendChild(uploadButton);
+                    clipContainer.appendChild(deleteButton);
+                    soundClips.appendChild(clipContainer);
+
+                    const blob = new Blob(chunks, {
+                        type: 'audio/ogg; codecs=opus',
+                    });
+                    chunks = [];
+                    const audioURl = URL.createObjectURL(blob);
+                    audio.src = audioURl;
+
+                    // eslint-disable-next-line no-shadow
+                    deleteButton.onclick = e => {
+                        const eventTarget = e.target;
+                        eventTarget.parentNode.parentNode.removeChild(eventTarget.parentNode);
+                    };
+
+                    // listen for file selection
+                    uploadButton.onclick = () => {};
+                };
+            },
+            // Error callback
+            err => console.log(`The following getUserMedia error occured: ${err}`)
+        )
+        .catch(err => console.log(`The following getUserMedia error occured: ${err}`));
+} else {
+    console.log('Sorry, getUserMedia not supported on your browser!');
+    alert('Sorry, getUserMedia not supported on your browser!');
+}
+
 const account = {
     name: 'quinn',
     sas: 'se=2019-06-01&sp=rwdlac&sv=2018-03-28&ss=b&srt=sco&sig=TzbhTXyTEQRmkcDN6zoHWgoVYRBDqdEaX2rmV0iTINA%3D',
