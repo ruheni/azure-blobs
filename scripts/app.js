870b1012c1fdeeb69234c9e47921eeaf38bd22bd
Ruheni Alex <aleruheni@outlook.com>
2019-04-30 17:02:10 +0300

    refactored code

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
-
 const account = {
     name: 'quinn',
     sas: 'se=2019-06-01&sp=rwdlac&sv=2018-03-28&ss=b&srt=sco&sig=TzbhTXyTEQRmkcDN6zoHWgoVYRBDqdEaX2rmV0iTINA%3D',
@@ -9,47 +8,58 @@ const account = {
 const blobUri = `https://${account.name}.blob.core.windows.net`;
 const blobService = AzureStorage.Blob.createBlobServiceWithSas(blobUri, account.sas);
 
-// Create a container on azure
+// Create container on Azure.
 const createContainer = () => {
     document.getElementById('create-button').addEventListener('click', () => {
         blobService.createContainerIfNotExists('container', (err, container) => {
             if (err) {
                 console.log('The following error occured when creating container: ', err);
             } else {
-                console.log(container.name);
-                alert(`${container.name} exists already.`);
+                console.log(`${container.name} already exists`);
+                alert(`${container.name} already exists`);
             }
         });
     });
 };
-createContainer();
 
-// Upload a blob file
+// Upload a blob file to Azure
 const uploadBlob = () => {
     document.getElementById('upload-button').addEventListener('click', () => {
+        /**
+         * TODO: change file to fetch specific audio file on browser.
+         */
         const file = document.getElementById('fileinput').files[0];
 
-        blobService.createBlockBlobFromBrowserFile('container', file.name, file, (err, result) => {
-            if (err) {
-                console.log('The following error occured while uploading blob: ', err);
-            } else {
-                console.log('Upload is successful');
-                alert('Upload successful.');
-            }
-        });
+        blobService.createBlockFromBrowserFile(
+            'container',
+            (err,
+            file.name,
+            file,
+            (err, result) => {
+                if (err) {
+                    console.log('The following error occured when uploading file: ', err);
+                } else {
+                    console.log(`Upload of ${file.name} successful.`);
+                    alert(`Upload of ${file.name} successful.`);
+                }
+            })
+        );
     });
 };
 
-// List Blobs
-// eslint-disable-next-line no-unused-vars
-const listBlobs = () => {
+// List blob files in container on Azure
+const listBlob = () => {
     document.getElementById('list-button').addEventListener('click', () => {
-        // const blobs = document.getElementById('blobs');
-        blobService.listBlobsSegmented('container', null, (err, results) => {
+        // eslint-disable-next-line no-unused-vars
+        const blobs = document.getElementById('blobs');
+        blobService.listBlobSegmented('container', null, (err, results) => {
             if (err) {
                 console.log('The following error occured while fetching blob: ', err);
             } else {
                 results.entries.forEach(blob => {
+                    /**
+                     * TODO: add blobs to web page, .blob section
+                     */
                     console.log(blob.name);
                 });
             }
@@ -57,8 +67,7 @@ const listBlobs = () => {
     });
 };
 
-// Delebe Blobs
-// eslint-disable-next-line no-unused-vars
+// Delete Blob file on Azure
 const deleteBlob = () => {
     document.getElementById('delete-button').addEventListener('click', () => {
         let blobName;
@@ -72,129 +81,91 @@ const deleteBlob = () => {
     });
 };
 
+// Record audio from browser
 const record = document.querySelector('.record');
 const stop = document.querySelector('.stop');
 const soundClips = document.querySelector('.sound-clips');
 
-// eslint-disable-next-line no-unused-vars
-const createEl = () => {
-    createContainer();
-    const clipName = prompt('Enter name for your audio clip');
-
-    const clipContainer = document.createElement('article');
-    const clipLabel = document.createElement('p');
-    const audio = document.createElement('audio');
-    const deleteButton = document.createElement('button');
-    const uploadButton = document.createElement('button');
-
-    clipContainer.classList.add('clip');
-    audio.setAttribute('controls', '');
-    uploadButton.textContent = 'Upload';
-    deleteButton.innerHTML = 'Delete';
-    clipLabel.innerHTML = clipName;
-
-    // clipContainer.setAttribute('class', 'row');
-    uploadButton.setAttribute('id', 'upload-button');
-    uploadButton.setAttribute('class', 'btn blue darken-1 right-align button');
-    deleteButton.setAttribute('class', 'btn blue darken-1 right-align button');
-
-    clipContainer.appendChild(audio);
-    clipContainer.appendChild(clipLabel);
-    clipContainer.appendChild(uploadButton);
-    clipContainer.appendChild(deleteButton);
-    soundClips.appendChild(clipContainer);
-};
-
-/**
- * Basic getUserMedia setup
- */
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
     console.log('getUserMedia supported');
-    navigator.mediaDevices
-        .getUserMedia({ audio: true })
-        .then(
-            stream => {
-                // create new instance of MediaRecorder() and pass stream to be captured
-                const mediaRecorder = new MediaRecorder(stream);
+    navigator.mediaDevices.getUserMedia({ audio: true }).then(
+        stream => {
+            // create new instance of MediaRecorder() and pass stream as param to be captured
+            const mediaRecorder = new MediaRecorder(stream);
 
-                // start recording the stream once record button is pressed
-                record.onclick = () => {
-                    mediaRecorder.start();
-                    console.log(mediaRecorder.state);
-                    console.log('recorder started');
-                    // record.style.background = 'red';
-                    record.style.color = 'red';
+            record.onclick = () => {
+                mediaRecorder.start();
+                console.log(`${mediaRecorder.state}, recorder started`);
+                record.style.color = 'red';
+            };
+
+            /**
+             * collect audio as recording progresses with
+             * on mediaRecorder.ondataavailable event
+             */
+            const chunks = [];
+            mediaRecorder.ondataavailable = e => chunks.push(e.data);
+
+            // finalize blob for use
+            stop.onclick = () => {
+                mediaRecorder.stop();
+                console.log(`${mediaRecorder.state}, recorder stopped`);
+                record.style.color = '';
+            };
+
+            // grabbing and using blob
+            mediaRecorder.onstop = () => {
+                const clipName = prompt('Enter the name of your audio clip: ');
+
+                const clipContainer = document.createElement('article');
+                const clipLabel = document.createElement('p');
+                const audio = document.createElement('audio');
+                const deleteButton = document.createElement('button');
+                const uploadButton = document.createElement('button');
+
+                clipContainer.classList.add('clip');
+                audio.setAttribute('controls', '');
+                uploadButton.textContent = 'Upload';
+                deleteButton.innerHTML = 'Delete';
+                clipLabel.innerHTML = clipName;
+
+                // clipContainer.setAttribute('class', 'row');
+                uploadButton.setAttribute('id', 'upload-button');
+                uploadButton.setAttribute('class', 'btn blue darken-1 right-align button');
+                deleteButton.setAttribute('class', 'btn blue darken-1 right-align button');
+
+                clipContainer.appendChild(audio);
+                clipContainer.appendChild(clipLabel);
+                clipContainer.appendChild(uploadButton);
+                clipContainer.appendChild(deleteButton);
+                soundClips.appendChild(clipContainer);
+
+                const blob = new Blob(chunks, {
+                    type: 'audio/ogg, codecs=opus',
+                });
+                // eslint-disable-next-line no-const-assign
+                chunks = [];
+                const audioURL = URL.createObjectURL(blob);
+                audio.src = audioURL;
+
+                // delete audio element
+                deleteButton.onclick = e => {
+                    const eventTarget = e.target;
+                    eventTarget.parentNode.parentNode.removeChild(eventTarget.parentNode);
                 };
 
-                /**
-                 * collect audio data as recording progresses with
-                 * on mediaRecorder.ondataavailable event
-                 */
-                let chunks = [];
-                mediaRecorder.ondataavailable = e => chunks.push(e.data);
-
-                // finalize Blob ready for use in app
-                stop.onclick = () => {
-                    mediaRecorder.stop();
-                    console.log(mediaRecorder.state);
-                    console.log('recorder stopped');
-                    record.style.background = '';
-                    record.style.color = '';
+                // upload blob
+                uploadButton.onclick = e => {
+                    const eventTarget = e.target;
+                    console.log(eventTarget);
+                    // uploadBlob();
                 };
-
-                // grabbing and using the blob
-                mediaRecorder.onstop = e => {
-                    console.log('recorder stopped');
-
-                    const clipName = prompt('Enter name for your audio clip');
-
-                    const clipContainer = document.createElement('article');
-                    const clipLabel = document.createElement('p');
-                    const audio = document.createElement('audio');
-                    const deleteButton = document.createElement('button');
-                    const uploadButton = document.createElement('button');
-
-                    clipContainer.classList.add('clip');
-                    audio.setAttribute('controls', '');
-                    uploadButton.textContent = 'Upload';
-                    deleteButton.innerHTML = 'Delete';
-                    clipLabel.innerHTML = clipName;
-
-                    // clipContainer.setAttribute('class', 'row');
-                    uploadButton.setAttribute('id', 'upload-button');
-                    uploadButton.setAttribute('class', 'btn blue darken-1 right-align button');
-                    deleteButton.setAttribute('class', 'btn blue darken-1 right-align button');
-
-                    clipContainer.appendChild(audio);
-                    clipContainer.appendChild(clipLabel);
-                    clipContainer.appendChild(uploadButton);
-                    clipContainer.appendChild(deleteButton);
-                    soundClips.appendChild(clipContainer);
-
-                    const blob = new Blob(chunks, {
-                        type: 'audio/ogg; codecs=opus',
-                    });
-                    chunks = [];
-                    const audioURl = URL.createObjectURL(blob);
-                    audio.src = audioURl;
-
-                    // eslint-disable-next-line no-shadow
-                    deleteButton.onclick = e => {
-                        const eventTarget = e.target;
-                        eventTarget.parentNode.parentNode.removeChild(eventTarget.parentNode);
-                    };
-
-                    // listen for file selection
-                    uploadButton.onclick = () => {
-                        uploadBlob();
-                    };
-                };
-            },
-            // Error callback
-            err => console.log(`The following getUserMedia error occured: ${err}`)
-        )
-        .catch(err => console.log(`The following getUserMedia error occured: ${err}`));
+            };
+        },
+        // error callback
+        err => console.log(`The following getUserMedia error occured: ${err}`)
+    );
 } else {
     console.log('Sorry, getUserMedia not supported on your browser!');
-    alert('Sorry, getUserMedia not supported on your browser!');
+    alert('Sorry, getUserMedia not supported on your browser...');
 }
